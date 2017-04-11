e162a83e15b81c448f2366998937051d04f19e45
gongdo <gongdo@live.com>
2017-04-11 16:06:25 +0900

    VS 2017, .NET Core 1.1.1 migration #13

--- samples/CoolSms.Samples.Console.Core/Program.cs
+++ samples/CoolSms.Samples.Console.Core/Program.cs
@@ -1,25 +1,38 @@
-﻿namespace CoolSms.Samples.Console.Core
+﻿using Microsoft.Extensions.Configuration;
+using System.Text;
+
+namespace CoolSms.Samples.Console.Core
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            // Platform codepage 지원
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile("appsettings.local.json", true)
+                .Build();
+
             // 옵션 설정.
             // 반드시 CoolSMS의 실제 설정과 동일한 값으로 설정해야 합니다.
             // 그렇지 않으면 SendMessageAsync에서 오류가 발생합니다.
             var client = new SmsApi(new SmsApiOptions
             {
-                ApiKey = "NCS56DE317B1631F",
-                ApiSecret = "6B50864BA7AFCD707BB4292FD1685C9E",
-                DefaultSenderId = "025189909",
+                ApiKey = configuration["coolSms:ApiKey"],
+                ApiSecret = configuration["coolSms:ApiSecret"],
+                DefaultSenderId = configuration["coolSms:DefaultSenderId"],
             });
 
             // 테스트 전송.
             // 실제로 발송되지는 않고 CoolSMS 서버에 등록되어 전송된 걸로 기록이 나오는 것.
-            //var result = client.SendTestMessageAsync("[바풀공부방]입금안내\n상품명:테스트\n금액:1000\n은행:기업은행\n가상계좌:123412341234\n예금주:주식회사바풀").Result;
+            var result = client.SendTestMessageAsync("내용 123123, 내용이 80byte를 넘기면 자동으로 LMS로 전송되며 내용중 앞 부분이 자동으로 제목이 됩니다.").Result;
 
             // 실제 전송.
-            var result = client.SendMessageAsync("01091678130", "입금안내\n상품명:테스트\n금액:1000\n은행:기업은행\n가상계좌:123412341234\n예금주:주식회사바풀").Result;
+            //var result = client.SendMessageAsync(
+            //    to: "000-000-0000",
+            //    text: "내용 123123, 내용이 80byte를 넘기면 자동으로 LMS로 전송되며 내용중 앞 부분이 자동으로 제목이 됩니다.").Result;
 
             System.Console.WriteLine(
 $@"전송 결과:
